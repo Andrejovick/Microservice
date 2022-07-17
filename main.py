5cac2ee2795fe95dfeb2bdfd70290d571083ebfa
Andrejovick <107487040+Andrejovick@users.noreply.github.com>
2022-07-17 23:26:08 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,74 @@
+import requests
+import json
+
+# AUTH
+
+# from requests.auth import HTTPBasicAuth
+# response = requests.get('https://jsonplaceholder.typicode.com / user, ',
+#             auth = HTTPBasicAuth('user', 'pass'))
+# print(response)
+
+co = str(input("Aku motodu potrebujete vyuzit? (get, delete, post alebo put) "))
+
+if co == "get":
+
+
+# GET
+    while True:
+        try:
+            id = int(input("Ake je ID prispevku, ktore potrebujete zobrazit? "))
+            response = requests.get(f'https://jsonplaceholder.typicode.com/posts/{id}')
+            print(response.json())
+            print(response.status_code)
+            break
+        except ValueError:
+            print("Musite zadat cele cislo ")                     
+
+elif co == "delete":
+
+
+# DELETE
+    while True:
+        try:
+            id = int(input("Ake je ID prispevku, ktore potrebujete vymazat? "))
+            response = requests.delete(f'https://jsonplaceholder.typicode.com/posts/{id}')
+            print(response.json())
+            print(response.status_code)
+            break
+        except ValueError:
+            print("Musite zadat cele cislo ")
+
+elif co == "post":
+
+
+# POST
+
+    user_id = int(input("ID pouzivatela: "))
+    post = str(input("Nazov noveho prispevku: "))
+    bodyy = str(input("Obsah noveho prispevku: "))
+    user_idd = {'userId': user_id}
+    prispevok = {'title': post}
+    obsah = {'body': bodyy}
+    mix = {'userId': user_id, 'title': post, 'body': bodyy}
+    headers = {"Content-type": "application/json"}
+    response = requests.post('https://jsonplaceholder.typicode.com/posts', json = mix, headers = headers)
+    print(response.json())
+    print(response.status_code)
+
+elif co == "put":
+
+
+# PUT
+
+    id = int(input("Ake je ID prispevku, ktoreho obsah potrebujete upravit? "))
+    post = str(input("Upraveny nazov prispevoku: "))
+    bodyy = str(input("Upraveny obsah prispevku: "))
+    prispevok = {'title': post}
+    obsah = {'body': bodyy}
+    mix1 = {'title': post, 'body': bodyy}
+    headers = {"Content-type": "application/json"}
+    response = requests.put(f'https://jsonplaceholder.typicode.com/posts/{id}', json = mix1, headers = headers)
+    print(response.json())
+    print(response.status_code)
+
+else: print("Zadat je mozne len jednu zo 4 metod: (get, delete, post alebo put) ")
---
+++ readme.md
@@ -0,0 +1,42 @@
+Requests
+
+Using windows:
+```CMD
+install requests
+```
+
+External API:
+https://jsonplaceholder.typicode.com/
+
+The available nested routes are:
+
+    /posts/1/comments
+    /albums/1/photos
+    /users/1/albums
+    /users/1/todos
+    /users/1/posts
+
+
+You can use your own local JSON server:
+
+Install JSON Server 
+
+```bash
+npm install -g json-server
+```
+
+Create a `db.json` file with some data
+
+Example>
+```json
+{
+  "post": [
+    { "id": 1, "title": "json-server", "body": "typicode", "userId": 1 }
+  ],
+}
+
+Start JSON Server
+
+```bash
+json-server --watch db.json
+```
